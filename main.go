54f0f1824594dc0191218a101e17ca9d531831c8
Ivan Shvedunov <ivan4th@gmail.com>
2016-04-20 15:19:55 +0300

    Improved -cpuprofile, added -die.
    CPU profiler now prints stats on exit.
    -die makes it possible to run wb-rules for the specified amount of time.

--- main.go
+++ main.go
@@ -3,14 +3,100 @@ package main
 import (
 	wbrules "./wbrules"
 	"flag"
+	"fmt"
 	"github.com/contactless/wbgo"
 	"os"
 	"os/signal"
-	"runtime/pprof"
+	"syscall"
 	"time"
+	// "runtime/debug"
+	MQTT "github.com/contactless/org.eclipse.paho.mqtt.golang"
+	"runtime/pprof"
 )
 
 const DRIVER_CLIENT_ID = "rules"
+
+func getRusage() (userTime time.Duration, sysTime time.Duration) {
+	var ru syscall.Rusage
+	if err := syscall.Getrusage(syscall.RUSAGE_SELF, &ru); err != nil {
+		fmt.Fprintln(os.Stderr, "*** -cpuprofile: Getrusage() failed")
+		return 0, 0
+	}
+	userTime = time.Duration(ru.Utime.Sec)*time.Second + time.Duration(ru.Utime.Usec)*time.Microsecond
+	sysTime = time.Duration(ru.Stime.Sec)*time.Second + time.Duration(ru.Stime.Usec)*time.Microsecond
+	return
+}
+
+// TBD: move to wbgo (together with flags)
+// also, make it possible to disable gc there using flags
+func profile(profFile string, dieAfter time.Duration, readyCh <-chan struct{}) {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	go func() {
+		if readyCh != nil {
+			<-readyCh
+		}
+		start := time.Now()
+		initialUserTime, initialSysTime := getRusage()
+		packetsInitiallySent, packetsInitiallyReceived := MQTT.GetStats()
+
+		f, err := os.Create(profFile)
+		if err != nil {
+			wbgo.Error.Fatalf("error creating profiling file: %s", err)
+		}
+		pprof.StartCPUProfile(f)
+
+		// TBD: add timer
+		var dieCh <-chan time.Time = nil
+		if dieAfter > 0 {
+			dieCh = time.After(dieAfter)
+		}
+		select {
+		case <-ch:
+		case <-dieCh:
+		}
+		newUserTime, newSysTime := getRusage()
+		elapsed := time.Since(start)
+		if elapsed == 0 {
+			fmt.Fprintln(os.Stderr, "oops, no time elapsed?")
+			return
+		}
+		elapsedUserTime := newUserTime - initialUserTime
+		elapsedSysTime := newSysTime - initialSysTime
+		elapsedCpuTime := elapsedUserTime + elapsedSysTime
+		cpuLoadUser := float64(elapsedUserTime) * 100.0 / float64(elapsed)
+		cpuLoadSys := float64(elapsedSysTime) * 100.0 / float64(elapsed)
+		cpuLoad := float64(elapsedCpuTime) * 100.0 / float64(elapsed)
+
+		newPacketsSent, newPacketsReceived := MQTT.GetStats()
+		packetsSent := newPacketsSent - packetsInitiallySent
+		packetsReceived := newPacketsReceived - packetsInitiallyReceived
+		if packetsSent > 0 || packetsReceived > 0 {
+			packetsSentPerSecond := float64(packetsSent) * float64(time.Second) /
+				float64(elapsed)
+			packetsReceivedPerSecond := float64(packetsReceived) * float64(time.Second) /
+				float64(elapsed)
+			packetsSentPerCpuSecond := float64(packetsSent) * float64(time.Second) /
+				float64(elapsedCpuTime)
+			packetsReceivedPerCpuSecond := float64(packetsReceived) * float64(time.Second) /
+				float64(elapsedCpuTime)
+
+			fmt.Printf("\n*** %d packets sent (%.2f per sec, %.2f per cpu sec), "+
+				"%d packets received (%.2f per sec, %.2f per cpu sec)\n",
+				packetsSent, packetsSentPerSecond, packetsSentPerCpuSecond,
+				packetsReceived, packetsReceivedPerSecond, packetsReceivedPerCpuSecond)
+		}
+
+		fmt.Printf("*** %.2f seconds elapsed, %.2f user, %.2f sys\n",
+			float64(elapsed)/float64(time.Second),
+			float64(elapsedUserTime)/float64(time.Second),
+			float64(elapsedSysTime)/float64(time.Second))
+		fmt.Printf("*** %.2f%% CPU load, %.2f%% user, %.2f%% sys\n",
+			cpuLoad, cpuLoadUser, cpuLoadSys)
+		pprof.StopCPUProfile()
+		os.Exit(130)
+	}()
+}
 
 func main() {
 	brokerAddress := flag.String("broker", "tcp://localhost:1883", "MQTT broker url")
@@ -18,6 +104,7 @@ func main() {
 	debug := flag.Bool("debug", false, "Enable debugging")
 	useSyslog := flag.Bool("syslog", false, "Use syslog for logging")
 	mqttDebug := flag.Bool("mqttdebug", false, "Enable MQTT debugging")
+	dieAfter := flag.Int("die", 0, "Die after specified number of seconds")
 	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to file")
 	flag.Parse()
 	if flag.NArg() < 1 {
@@ -63,22 +150,12 @@ func main() {
 		rpc.Start()
 	}
 
+	engine.Start()
+
 	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			wbgo.Error.Fatalf("error creating profiling file: %s", err)
-		}
-		pprof.StartCPUProfile(f)
-		ch := make(chan os.Signal, 1)
-		signal.Notify(ch, os.Interrupt)
-		go func() {
-			<-ch
-			pprof.StopCPUProfile()
-			os.Exit(130)
-		}()
+		profile(*cpuprofile, time.Duration(*dieAfter)*time.Second, engine.ReadyCh())
 	}
 
-	engine.Start()
 	for {
 		time.Sleep(1 * time.Second)
 	}
