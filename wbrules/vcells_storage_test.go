b97e8e729430f53623c923755253b375f67dd4dd
Nikita webconn Maslov <n.maslov@contactless.ru>
2017-01-13 12:50:32 +0300

    vcells-storage: fix test
    Signed-off-by: Nikita webconn Maslov <n.maslov@contactless.ru>

--- wbrules/vcells_storage_test.go
+++ wbrules/vcells_storage_test.go
@@ -36,11 +36,34 @@ func (s *VirtualCellsStorageSuite) TearDownTest() {
 }
 
 func (s *VirtualCellsStorageSuite) TestStorage1() {
-	s.publish("/devices/test-trigger/controls/echo/on", "1", "test-trigger/echo")
+	// s.publish("/devices/test-trigger/controls/echo/on", "1", "test-trigger/echo")
+	// s.Verify(
+	// "tst -> /devices/test-trigger/controls/echo/on: [1] (QoS 1)",
+	// "driver -> /devices/test-trigger/controls/echo: [1] (QoS 1)",
+	// "[info] vdev false, true, false, foo",
+	// )
+	// s.VerifyEmpty()
 
-	s.VerifyUnordered(
-		"[info] vdev 0, 1, 0, foo",
+	s.publish("/devices/test-trigger/controls/change1/on", "1", "test-trigger/change1",
+		"test-vdev/cell1", "test-vdev/cell3", "test-vdev/cellText")
+	s.Verify(
+		"tst -> /devices/test-trigger/controls/change1/on: [1] (QoS 1)",
+		"driver -> /devices/test-trigger/controls/change1: [1] (QoS 1)",
+		"driver -> /devices/test-vdev/controls/cell1: [1] (QoS 1, retained)",
+		"driver -> /devices/test-vdev/controls/cell3: [1] (QoS 1, retained)",
+		"driver -> /devices/test-vdev/controls/cellText: [bar] (QoS 1, retained)",
 	)
+	s.VerifyEmpty()
+}
+
+func (s *VirtualCellsStorageSuite) TestStorage2() {
+	s.publish("/devices/test-trigger/controls/echo/on", "1", "test-trigger/echo")
+	s.Verify(
+		"tst -> /devices/test-trigger/controls/echo/on: [1] (QoS 1)",
+		"driver -> /devices/test-trigger/controls/echo: [1] (QoS 1)",
+		"[info] vdev true, true, false, bar",
+	)
+	s.VerifyEmpty()
 }
 
 func TestVirtualCellsStorageSuite(t *testing.T) {
