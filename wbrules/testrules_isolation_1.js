500334e8cc43fa3866955e9fe74dbc802205417e
Nikita webconn Maslov <n.maslov@contactless.ru>
2017-01-23 17:00:52 +0300

    test: upgrade isolation test
    Signed-off-by: Nikita webconn Maslov <n.maslov@contactless.ru>

--- wbrules/testrules_isolation_1.js
+++ wbrules/testrules_isolation_1.js
@@ -8,9 +8,11 @@ defineVirtualDevice("vdev", {
   }
 });
 
+var v = 84;
+
 defineRule("isolated_rule", {
   whenChanged: ["vdev/someCell"],
   then: function () {
-    log("isolated_rule (testrules_isolation_1.js)");
+    log("isolated_rule (testrules_isolation_1.js) " + v);
   }
 });
