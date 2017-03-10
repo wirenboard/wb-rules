1150907e53c90f4205a0061ebd3a71edc5e26ec8
Nikita webconn Maslov <n.maslov@contactless.ru>
2017-03-10 16:53:04 +0300

    test: add rule controls test
    Signed-off-by: Nikita webconn Maslov <n.maslov@contactless.ru>

---
+++ wbrules/rule_controls_test.go
@@ -0,0 +1,62 @@
+package wbrules
+
+import (
+	"github.com/contactless/wbgo/testutils"
+	"testing"
+)
+
+type RuleControlsSuite struct {
+	RuleSuiteBase
+}
+
+func (s *RuleControlsSuite) SetupTest() {
+	s.SetupSkippingDefs("testrules_rule_controls.js")
+}
+
+func (s *RuleControlsSuite) TestTrigger() {
+	s.publish("/devices/ctrltest/controls/trigger/on", "1", "ctrltest/trigger")
+
+	s.Verify(
+		"tst -> /devices/ctrltest/controls/trigger/on: [1] (QoS 1)",
+		"driver -> /devices/ctrltest/controls/trigger: [1] (QoS 1)",
+		"[info] controllable rule fired",
+	)
+}
+
+func (s *RuleControlsSuite) TestDisable() {
+	s.publish("/devices/ctrltest/controls/disable/on", "1", "ctrltest/disable")
+
+	s.Verify(
+		"tst -> /devices/ctrltest/controls/disable/on: [1] (QoS 1)",
+		"driver -> /devices/ctrltest/controls/disable: [1] (QoS 1)",
+		"[info] disable",
+	)
+
+	s.publish("/devices/ctrltest/controls/trigger/on", "1", "ctrltest/trigger")
+	s.Verify(
+		"tst -> /devices/ctrltest/controls/trigger/on: [1] (QoS 1)",
+		"driver -> /devices/ctrltest/controls/trigger: [1] (QoS 1)",
+	)
+
+	s.VerifyEmpty()
+
+	s.publish("/devices/ctrltest/controls/enable/on", "1", "ctrltest/enable")
+
+	s.Verify(
+		"tst -> /devices/ctrltest/controls/enable/on: [1] (QoS 1)",
+		"driver -> /devices/ctrltest/controls/enable: [1] (QoS 1)",
+		"[info] enable",
+	)
+
+	s.publish("/devices/ctrltest/controls/trigger/on", "1", "ctrltest/trigger")
+
+	s.Verify(
+		"tst -> /devices/ctrltest/controls/trigger/on: [1] (QoS 1)",
+		"driver -> /devices/ctrltest/controls/trigger: [1] (QoS 1)",
+		"[info] controllable rule fired",
+	)
+}
+
+func TestRuleControls(t *testing.T) {
+	testutils.RunSuites(t, new(RuleControlsSuite))
+}
