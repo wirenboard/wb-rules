ed6adb53f33cdd818f9ceac8c83b3b9b8e118610
Nikita webconn Maslov <n.maslov@contactless.ru>
2017-05-22 15:29:10 +0300

    wb-rules: make queues len variable
    Signed-off-by: Nikita webconn Maslov <n.maslov@contactless.ru>

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ const (
 	PERSISTENT_DB_FILE    = "/var/lib/wirenboard/wbrules-persistent.db"
 	VIRTUAL_CELLS_DB_FILE = "/var/lib/wirenboard/wbrules-vcells.db"
 
+	MQTT_QUEUE_LEN = 2048
+
 	WBRULES_MODULES_ENV = "WBRULES_MODULES"
 )
 
@@ -24,6 +26,8 @@ func main() {
 	debug := flag.Bool("debug", false, "Enable debugging")
 	useSyslog := flag.Bool("syslog", false, "Use syslog for logging")
 	mqttDebug := flag.Bool("mqttdebug", false, "Enable MQTT debugging")
+	queueLens := flag.Uint("queue-len", MQTT_QUEUE_LEN, "Set MQTT inner queues lengths")
+
 	flag.Parse()
 	if flag.NArg() < 1 {
 		wbgo.Error.Fatal("must specify rule file/directory name(s)")
@@ -38,7 +42,7 @@ func main() {
 		wbgo.EnableMQTTDebugLog(*useSyslog)
 	}
 	model := wbrules.NewCellModel()
-	mqttClient := wbgo.NewPahoMQTTClient(*brokerAddress, DRIVER_CLIENT_ID, true)
+	mqttClient := wbgo.NewPahoMQTTClientQueues(*brokerAddress, DRIVER_CLIENT_ID, true, *queueLens, *queueLens)
 	driver := wbgo.NewDriver(model, mqttClient)
 	driver.SetAutoPoll(false)
 	driver.SetAcceptsExternalDevices(true)
