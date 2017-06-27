6aa436c4ed991d9fdd711005462f93b113b0b97c
Nikita webconn Maslov <n.maslov@contactless.ru>
2017-06-27 18:39:01 +0300

    engine: fix timer quit channel close on fire
    Signed-off-by: Nikita webconn Maslov <n.maslov@contactless.ru>

--- wbrules/engine.go
+++ wbrules/engine.go
@@ -725,6 +725,7 @@ func (engine *RuleEngine) StartTimer(name string, callback func(), interval time
 						}
 					})
 					if !periodic {
+						close(entry.quitted)
 						return
 					}
 				case <-entry.quit:
