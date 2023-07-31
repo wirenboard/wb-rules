23ecfc7819576f6cd10f715044be665aebbd8926
Ekaterina Volkova <ekaterina.volkova@wirenboard.ru>
2023-07-31 10:34:22 +0300

    Replace warning to debug log level of uncomplete control message

--- wbrules/engine.go
+++ wbrules/engine.go
@@ -836,7 +836,7 @@ func (engine *RuleEngine) driverEventHandler(event wbgong.DriverEvent) {
 
 		value, err = ctrl.GetValue()
 		if err != nil {
-			wbgong.Warn.Printf("%s: failed to convert value '%s', passing raw, error: %s",
+			wbgong.Debug.Printf("%s: failed to convert value '%s', passing raw, error: %s",
 				spec.String(), ctrl.GetRawValue(), err.Error())
 			value = ctrl.GetRawValue()
 		}
