a3bffa7c84ecbb755c6b0825485bea9acd5b567f
Nikolay Korotkiy <sikmir@disroot.org>
2024-09-18 10:59:38 +0400

    Fix device meta cleanup (#123)

--- wbrules/rule_reload_test.go
+++ wbrules/rule_reload_test.go
@@ -87,6 +87,7 @@ func (s *RuleReloadSuite) TestReload() {
 		"driver -> /devices/vdev/controls/someCell: [1] (QoS 1, retained)",
 		"driver -> /devices/vdev/controls/someCell: [] (QoS 1, retained)",
 		"driver -> /devices/vdev/meta: [{\"driver\":\"wbrules\",\"title\":{\"en\":\"VDev\"}}] (QoS 1, retained)",
+		"driver -> /devices/vdev/meta: [] (QoS 1, retained)",
 		"driver -> /devices/vdev/meta/driver: [] (QoS 1, retained)",
 		"driver -> /devices/vdev/meta/driver: [wbrules] (QoS 1, retained)",
 		"driver -> /devices/vdev/meta/name: [VDev] (QoS 1, retained)",
@@ -98,6 +99,7 @@ func (s *RuleReloadSuite) TestReload() {
 		"driver -> /devices/vdev1/controls/qqq: [] (QoS 1, retained)",
 		"driver -> /devices/vdev1/meta/driver: [] (QoS 1, retained)",
 		"driver -> /devices/vdev1/meta/name: [] (QoS 1, retained)",
+		"driver -> /devices/vdev1/meta: [] (QoS 1, retained)",
 		"wbrules-log -> /wbrules/updates/changed: [testrules_reload_2.js] (QoS 1)",
 	)
 	// rules are run after reload
@@ -154,6 +156,7 @@ func (s *RuleReloadSuite) TestRemoveScript() {
 		"driver -> /devices/vdev/controls/someCell: [] (QoS 1, retained)",
 		"driver -> /devices/vdev/meta/driver: [] (QoS 1, retained)",
 		"driver -> /devices/vdev/meta/name: [] (QoS 1, retained)",
+		"driver -> /devices/vdev/meta: [] (QoS 1, retained)",
 		"driver -> /devices/vdev1/controls/qqq/meta/order: [] (QoS 1, retained)",
 		"driver -> /devices/vdev1/controls/qqq/meta/readonly: [] (QoS 1, retained)",
 		"driver -> /devices/vdev1/controls/qqq/meta/type: [] (QoS 1, retained)",
@@ -161,6 +164,7 @@ func (s *RuleReloadSuite) TestRemoveScript() {
 		"driver -> /devices/vdev1/controls/qqq: [] (QoS 1, retained)",
 		"driver -> /devices/vdev1/meta/driver: [] (QoS 1, retained)",
 		"driver -> /devices/vdev1/meta/name: [] (QoS 1, retained)",
+		"driver -> /devices/vdev1/meta: [] (QoS 1, retained)",
 		"wbrules-log -> /wbrules/updates/removed: [testrules_reload_2.js] (QoS 1)",
 	)
 
@@ -235,6 +239,7 @@ func (s *RuleReloadSuite) TestRemoveRestore() {
 		"driver -> /devices/vdev/controls/someCell: [] (QoS 1, retained)",
 		"driver -> /devices/vdev/meta/driver: [] (QoS 1, retained)",
 		"driver -> /devices/vdev/meta/name: [] (QoS 1, retained)",
+		"driver -> /devices/vdev/meta: [] (QoS 1, retained)",
 		"driver -> /devices/vdev1/controls/qqq/meta/order: [] (QoS 1, retained)",
 		"driver -> /devices/vdev1/controls/qqq/meta/readonly: [] (QoS 1, retained)",
 		"driver -> /devices/vdev1/controls/qqq/meta/type: [] (QoS 1, retained)",
@@ -242,6 +247,7 @@ func (s *RuleReloadSuite) TestRemoveRestore() {
 		"driver -> /devices/vdev1/controls/qqq: [] (QoS 1, retained)",
 		"driver -> /devices/vdev1/meta/driver: [] (QoS 1, retained)",
 		"driver -> /devices/vdev1/meta/name: [] (QoS 1, retained)",
+		"driver -> /devices/vdev1/meta: [] (QoS 1, retained)",
 		"wbrules-log -> /wbrules/updates/removed: [testrules_reload_2.js] (QoS 1)")
 
 	// load script and expect vdev definition at least
@@ -313,6 +319,7 @@ func (s *RuleReloadSuite) TestDisableScript() {
 		"driver -> /devices/vdev0/controls/someCell: [] (QoS 1, retained)",
 		"driver -> /devices/vdev0/meta/driver: [] (QoS 1, retained)",
 		"driver -> /devices/vdev0/meta/name: [] (QoS 1, retained)",
+		"driver -> /devices/vdev0/meta: [] (QoS 1, retained)",
 		"timer.Stop(): 1",
 		"wbrules-log -> /wbrules/updates/removed: [testrules_reload_1.js] (QoS 1)",
 	)
