632d04f9471d6fe9b9df39fdee552607b1166d7a
Nikolay Korotkiy <sikmir@disroot.org>
2026-05-30 22:51:51 +0400

    trackMqtt: pass 'retained' and 'qos' parameters to callback function

--- wbrules/testrules_track_mqtt.js
+++ wbrules/testrules_track_mqtt.js
@@ -1,19 +1,19 @@
 trackMqtt('/wierd/sub/some', function (obj) {
   log('1. wierd topic got value');
-  log('topic: {}, value: {}'.format(obj.topic, obj.value));
+  log('topic: {}, value: {}, retained: {}, qos: {}'.format(obj.topic, obj.value, obj.retained, obj.qos));
 });
 
 trackMqtt('/wierd/+/some', function (obj) {
   log('2. wierd topic got value');
-  log('topic: {}, value: {}'.format(obj.topic, obj.value));
+  log('topic: {}, value: {}, retained: {}, qos: {}'.format(obj.topic, obj.value, obj.retained, obj.qos));
 });
 
 trackMqtt('/wierd/+/another', function (obj) {
   log('3. wierd topic got value');
-  log('topic: {}, value: {}'.format(obj.topic, obj.value));
+  log('topic: {}, value: {}, retained: {}, qos: {}'.format(obj.topic, obj.value, obj.retained, obj.qos));
 });
 
 trackMqtt('/wierd/#', function (obj) {
   log('4. wierd topic got value');
-  log('topic: {}, value: {}'.format(obj.topic, obj.value));
+  log('topic: {}, value: {}, retained: {}, qos: {}'.format(obj.topic, obj.value, obj.retained, obj.qos));
 });
