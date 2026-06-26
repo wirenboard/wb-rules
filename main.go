00bf953c1b4383fa093407ac33876ec867fd0553
Nikolay Korotkiy <sikmir@disroot.org>
2026-06-26 10:53:03 +0400

    Editor: reject saving hidden files and ignore them in the directory watcher (#205)

--- main.go
+++ main.go
@@ -82,7 +82,7 @@ func main() {
 		http.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request) {
 			metrics.WritePrometheus(w, true)
 		})
-		// debug/pprof handlers are registered in https://cs.opensource.google/go/go/+/refs/tags/go1.21.0:src/net/http/pprof/pprof.go;l=93
+		// debug/pprof handlers are registered in https://cs.opensource.google/go/go/+/refs/tags/go1.24.0:src/net/http/pprof/pprof.go;l=95
 		go func() {
 			log.Println(http.ListenAndServe(*httpAddr, nil))
 		}()
@@ -158,7 +158,7 @@ func main() {
 	defer engine.Stop()
 
 	gotSome := false
-	watcher := wbgong.NewDirWatcher("\\.js(\\"+wbrules.FILE_DISABLED_SUFFIX+")?$", engine)
+	watcher := wbgong.NewDirWatcher("(^|/)[^/.][^/]*\\.js(\\"+wbrules.FILE_DISABLED_SUFFIX+")?$", engine)
 	if *editDir != "" {
 		err := engine.SetSourceRoot(*editDir)
 		if err != nil {
